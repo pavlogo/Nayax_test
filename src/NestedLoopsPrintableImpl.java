fb829eb4106778390ad64b1cfe5fa81ef68e3251
--replace-all <pavlo.gorduz@gmail.com>
2022-01-14 10:28:44 +0200

    add 'if' cond for null case

--- src/NestedLoopsPrintableImpl.java
+++ src/NestedLoopsPrintableImpl.java
@@ -2,17 +2,21 @@ public class NestedLoopsPrintableImpl implements MatrixPrintable {
 
     public static void print(int[][] data) throws NullPointerException {
 
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; (data[i] != null && j < data[i].length); j++) {
-                System.out.print(data[i][j] + " ");
+        if (data == null) {
+            System.out.println("null");
+        } else {
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; (data[i] != null && j < data[i].length); j++) {
+                    System.out.print(data[i][j] + " ");
+                }
+                if (data[i] != null && data[i].length == 0) {
+                    System.out.print("empty ");
+                }
+                if (data[i] == null) {
+                    System.out.print("null ");
+                }
+                System.out.println();
             }
-            if (data[i] != null && data[i].length == 0) {
-                System.out.print("empty ");
-            }
-            if (data[i] == null) {
-                System.out.print("null ");
-            }
-            System.out.println();
         }
     }
 }
--- src/SingleLoopPrintableImpl.java
+++ src/SingleLoopPrintableImpl.java
@@ -3,23 +3,27 @@ public class SingleLoopPrintableImpl implements MatrixPrintable {
 
     public static void print(int[][] data) throws NullPointerException {
 
-        for (int j = 0, i = 0; j < data.length; ) {
+        if (data == null) {
+            System.out.println("null");
+        } else {
+            for (int j = 0, i = 0; j < data.length; ) {
 
-            if (data[j] != null && data[j].length == 0) {
-                System.out.print("\n" + "empty " + "\n");
-            }
-            if (data[j] == null) {
-                System.out.print("\n" + "null " + "\n");
-            }
+                if (data[j] != null && data[j].length == 0) {
+                    System.out.print("\n" + "empty " + "\n");
+                }
+                if (data[j] == null) {
+                    System.out.print("\n" + "null " + "\n");
+                }
 
-            if (data[j] != null && i < data[j].length) {
+                if (data[j] != null && i < data[j].length) {
 
-                System.out.print(data[j][i] + " ");
-                ++i;
+                    System.out.print(data[j][i] + " ");
+                    i++;
 
-            } else {
-                i = 0;
-                ++j;
+                } else {
+                    i = 0;
+                    j++;
+                }
             }
         }
     }
